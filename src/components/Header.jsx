b3f685be35a571f9a593d6703dade6d4e8a39bf5
Wan Muzaffar <wanmuz86@gmail.com>
2025-03-26 09:05:45 +0700

    Add Link in the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div>
+        <h1>E-commerce Demo App</h1>
+        <nav>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About Us</Link></li>
+                <li><Link to="/contact">Contact</Link></li>
+            </ul>
+        </nav>
+    </div>
   )
 }
 
