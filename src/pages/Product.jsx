a6da800876fb4f4be1d3879532df4a439b712e85
Wan Muzaffar <wanmuz86@gmail.com>
2025-03-26 10:15:07 +0700

    add useNavigate

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
-import {useParams} from 'react-router-dom'
+import {useParams,useNavigate} from 'react-router-dom'
 
 const Product = () => {
     
     // Object destructuring
     const {productId} = useParams()
+    const navigate  = useNavigate()
+
     if (productId > 194){
         throw new Error('Product not found')
     }
+
+    const navigateHome = () => {
+        // Navigate to Home Page
+        navigate('/')
+    }
   return (
-    <div>Product Page for id {productId} </div>
+    <div>
+       <h1> Product Page for id {productId} </h1>
+       <button onClick={navigateHome}>Navigate Home</button>
+        
+        </div>
   )
 }
 
