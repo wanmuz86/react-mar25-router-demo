f9836afec8e57eeb5d81c59ceea60f34deaede4b
Wan Muzaffar <wanmuz86@gmail.com>
2025-03-26 09:53:48 +0700

    Add Query params

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import {useSearchParams} from 'react-router-dom'
 
 const ProductList = () => {
+    const [searchParams, setSearchParams] = useSearchParams()
+
+    useEffect(() =>{
+        console.log(searchParams.get('category'))
+        console.log(searchParams.get('lang'))
+    },[])
   return (
     <div>ProductList</div>
   )
