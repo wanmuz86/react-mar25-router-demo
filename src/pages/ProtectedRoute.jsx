e2d8a7f117a98388a75a2be295ffb5a5b90e4755
Wan Muzaffar <wanmuz86@gmail.com>
2025-03-26 13:12:34 +0700

    add protected route

--- src/pages/ProtectedRoute.jsx
+++ src/pages/ProtectedRoute.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Navigate, Outlet } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
 
 // Block or access user based on the token
 const ProtectedRoute = () => {
-    const navigate = useNavigate()
+    // const navigate = useNavigate()
     const localStorage = window.localStorage.getItem('token')
-    return localStorage ? <Outlet/> : navigate('/')
+    return localStorage ? <Outlet/> : <Navigate to="/" replace="/"/>
  
 }
 
